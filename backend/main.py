602cd19b70ee9a4a83e293c1734c652cbd00ee42
Brandon Martin-ANderson <bmander@wideworld.media.mit.edu>
2013-03-25 19:42:47 -0400

    nearby and bounds web methods

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,30 @@
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        name = self.get_argument( "name" )
-        self.write("Hello, "+name)
+from graphserver.ext.osm.osmdb import OSMDB
+from graphserver.graphdb import GraphDatabase
 
+OSMDB_FN = "/home/bmander/data/boston.osmdb"
+GRAPHDB_FN = "/home/bmander/data/boston.gdb"
+osmdb = OSMDB( OSMDB_FN )
+graphdb = GraphDatabase( GRAPHDB_FN )
+
+class BoundsHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write( {'bounds':osmdb.bounds()} )
+
+class NearbyHandler(tornado.web.RequestHandler):
+    def get(self):
+	lat = float(self.get_argument("lat"))
+        lon = float(self.get_argument("lon"))
+
+        node_id,node_lat,node_lon,node_dist = osmdb.nearest_node(lat,lon)
+
+        self.write( {'id':node_id,'lat':node_lat,'lon':node_lon,'dist':node_dist} )
 
 application = tornado.web.Application([
-    (r"/", MainHandler),
+    (r"/bounds", BoundsHandler),
+    (r"/nearby", NearbyHandler),
 ])
 
 if __name__ == "__main__":
