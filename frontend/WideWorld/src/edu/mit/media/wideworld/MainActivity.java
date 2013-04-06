1725f59ed1259eea83c3bf94bdf9665aae1d1476
Brandon Martin-Anderson <badhill@gmail.com>
2013-04-05 21:49:39 -0400

    actually go and get the json for the orute

--- frontend/WideWorld/src/edu/mit/media/wideworld/MainActivity.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/MainActivity.java
@@ -1,13 +1,20 @@
 package edu.mit.media.wideworld;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import edu.mit.media.wideworld.R;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
@@ -25,10 +32,18 @@ import android.view.GestureDetector;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Overlay;
 
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
 public class MainActivity extends Activity{
 	
 	IGeoPoint orig;
@@ -38,6 +53,10 @@ public class MainActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		/* TODO: REMOVE AS SOON AS POSSIBLE */
+		ThreadPolicy tp = ThreadPolicy.LAX;
+		StrictMode.setThreadPolicy(tp);
 		
 		RelativeLayout rl = (RelativeLayout) findViewById(R.id.root);
 		
@@ -60,8 +79,50 @@ public class MainActivity extends Activity{
 		return true;
 	}
 	
-	private void findAndDisplayRoute(){
+	private void findAndDisplayRoute() {
+		if( orig==null || dest==null ){
+			return;
+		}
+		
 		Log.v("DEBUG", "orig:"+orig+" dest:"+dest);
+		
+		HttpClient hc = new DefaultHttpClient();
+		HttpResponse response;
+		try {
+			double lat1 = orig.getLatitudeE6()/1E6;
+			double lng1 = orig.getLongitudeE6()/1E6;
+			double lat2 = dest.getLatitudeE6()/1E6;
+			double lng2 = dest.getLongitudeE6()/1E6;
+			
+			URI uri = new URI("http://wideworld.media.mit.edu/path?lat1="+lat1+"&lon1="+lng1+"&lat2="+lat2+"&lon2="+lng2);
+			Log.v("DEBUG",uri.toString());
+			response = hc.execute( new HttpGet( uri ));
+			 StatusLine statusLine = response.getStatusLine();
+		    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+		        ByteArrayOutputStream out = new ByteArrayOutputStream();
+		        response.getEntity().writeTo(out);
+		        out.close();
+		        String responseString = out.toString();
+		        Log.v("DEBUG", Integer.toString(responseString.length()));
+		        //..more logic
+		    } else{
+		        //Closes the connection.
+		        response.getEntity().getContent().close();
+		        throw new IOException(statusLine.getReasonPhrase());
+		    }
+			//Log.v("DEBUG", "resp:"+response);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+ catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public class GestureOverlay extends Overlay {
@@ -70,9 +131,7 @@ public class MainActivity extends Activity{
 
 		public GestureOverlay(Context ctx) {
 			super(ctx);
-			
 			context = ctx;
-			// TODO Auto-generated constructor stub
 		}
 
 		@Override
@@ -82,10 +141,9 @@ public class MainActivity extends Activity{
 		}
 		
         public boolean onLongPress(final MotionEvent e, final MapView mapView) {
-        	Log.v("DEBUG", e.toString());
         	final IGeoPoint pt = mapView.getProjection().fromPixels(e.getX(), e.getY());
-        	Log.v("DEBUG", pt.getLatitudeE6()+","+pt.getLongitudeE6());
         	
+        	// build alert dialog
         	AlertDialog.Builder builder = new AlertDialog.Builder(context);
         	builder.setPositiveButton("Destination", new DialogInterface.OnClickListener(){
         		public void onClick(DialogInterface dialog, int id){
@@ -101,6 +159,8 @@ public class MainActivity extends Activity{
         	});
         	builder.setCancelable(true);
         	AlertDialog ad = builder.create();
+        	
+        	// show alert dialog
         	ad.show();
         	
             return false;
