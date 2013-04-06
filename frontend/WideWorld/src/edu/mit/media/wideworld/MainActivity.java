138be5e7203293b5838578ac480510796aaa59e4
Brandon Martin-Anderson <badhill@gmail.com>
2013-04-05 21:22:57 -0400

    long press to choose origin and destination

--- frontend/WideWorld/src/edu/mit/media/wideworld/MainActivity.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/MainActivity.java
@@ -9,7 +9,12 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.MotionEventCompat;
@@ -25,6 +30,9 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Overlay;
 
 public class MainActivity extends Activity{
+	
+	IGeoPoint orig;
+	IGeoPoint dest;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +60,10 @@ public class MainActivity extends Activity{
 		return true;
 	}
 	
+	private void findAndDisplayRoute(){
+		Log.v("DEBUG", "orig:"+orig+" dest:"+dest);
+	}
+	
 	public class GestureOverlay extends Overlay {
 		
 		Context context;
@@ -71,14 +83,30 @@ public class MainActivity extends Activity{
 		
         public boolean onLongPress(final MotionEvent e, final MapView mapView) {
         	Log.v("DEBUG", e.toString());
-        	IGeoPoint pt = mapView.getProjection().fromPixels(e.getX(), e.getY());
+        	final IGeoPoint pt = mapView.getProjection().fromPixels(e.getX(), e.getY());
         	Log.v("DEBUG", pt.getLatitudeE6()+","+pt.getLongitudeE6());
         	
-        	
+        	AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        	builder.setPositiveButton("Destination", new DialogInterface.OnClickListener(){
+        		public void onClick(DialogInterface dialog, int id){
+        			dest = pt;
+        			findAndDisplayRoute();
+        		}
+        	});
+        	builder.setNegativeButton("Origin", new DialogInterface.OnClickListener(){
+        		public void onClick(DialogInterface dialog, int id){
+        			orig = pt;
+        			findAndDisplayRoute();
+        		}
+        	});
+        	builder.setCancelable(true);
+        	AlertDialog ad = builder.create();
+        	ad.show();
         	
             return false;
         }
-		
+        
+        
 	}
 	
 
