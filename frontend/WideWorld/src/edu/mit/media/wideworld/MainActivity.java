3ffff63268ce4bdd8dcba75e80e8f1a43f955023
Brandon Martin-Anderson <badhill@gmail.com>
2013-04-05 20:18:18 -0400

    get long press coordinates on long press

--- frontend/WideWorld/src/edu/mit/media/wideworld/MainActivity.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/MainActivity.java
@@ -1,18 +1,31 @@
 package edu.mit.media.wideworld;
 
+import java.io.IOException;
+import java.util.List;
+
 import edu.mit.media.wideworld.R;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
+import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Overlay;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity{
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,6 +40,9 @@ public class MainActivity extends Activity {
 		rl.addView(mv, new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT,
                 LayoutParams.MATCH_PARENT));
 		
+		Overlay overlay = new GestureOverlay(this);
+		mv.getOverlays().add(overlay);
+		
 	}
 
 	@Override
@@ -35,5 +51,35 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public class GestureOverlay extends Overlay {
+		
+		Context context;
+
+		public GestureOverlay(Context ctx) {
+			super(ctx);
+			
+			context = ctx;
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		protected void draw(Canvas arg0, MapView arg1, boolean arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+        public boolean onLongPress(final MotionEvent e, final MapView mapView) {
+        	Log.v("DEBUG", e.toString());
+        	IGeoPoint pt = mapView.getProjection().fromPixels(e.getX(), e.getY());
+        	Log.v("DEBUG", pt.getLatitudeE6()+","+pt.getLongitudeE6());
+        	
+        	
+        	
+            return false;
+        }
+		
+	}
+	
 
 }
