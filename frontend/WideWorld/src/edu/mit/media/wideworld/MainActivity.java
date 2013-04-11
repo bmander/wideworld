bff913e9e8ff146ddfdbb935a222442e6fcbeb70
Brandon Martin-Anderson <badhill@gmail.com>
2013-04-11 15:25:34 -0400

    the offset bug haunts us no longer

--- frontend/WideWorld/src/edu/mit/media/wideworld/MainActivity.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/MainActivity.java
@@ -1,229 +1,38 @@
 package edu.mit.media.wideworld;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-
-import edu.mit.media.wideworld.R;
-
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.os.StrictMode.ThreadPolicy;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.support.v4.view.GestureDetectorCompat;
-import android.support.v4.view.MotionEventCompat;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MotionEvent;
-import android.view.GestureDetector;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.osmdroid.api.IGeoPoint;
-import org.osmdroid.mapsforge.wrapper.MapController;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.Overlay;
-import org.osmdroid.views.overlay.PathOverlay;
+public class MainActivity extends FragmentActivity
+{
 
-@TargetApi(Build.VERSION_CODES.GINGERBREAD)
-public class MainActivity extends Activity{
-	
-	IGeoPoint orig;
-	IGeoPoint dest;
-	MapView mv;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        this.setContentView(R.layout.activity_main);
+        
 		/* TODO: REMOVE AS SOON AS POSSIBLE */
 		ThreadPolicy tp = ThreadPolicy.LAX;
 		StrictMode.setThreadPolicy(tp);
-		
-		RelativeLayout rl = (RelativeLayout) findViewById(R.id.root);
-		
-		// set up map
-		mv = new MapView(this, 256);
-		org.osmdroid.views.MapController mc = mv.getController();
-		mc.setZoom(13);
-		mc.setCenter(new GeoPoint(42.360346,-71.088105));
-		mv.setBuiltInZoomControls(true);
-		rl.addView(mv, new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT,
-                LayoutParams.MATCH_PARENT));
-		
-		Overlay overlay = new GestureOverlay(this);
-		mv.getOverlays().add(overlay);
-		
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	private void findAndDisplayRoute() {
-		if( orig==null || dest==null ){
-			return;
-		}
-		
-		// get the orig and dest points
-		double lat1 = orig.getLatitudeE6()/1E6;
-		double lng1 = orig.getLongitudeE6()/1E6;
-		double lat2 = dest.getLatitudeE6()/1E6;
-		double lng2 = dest.getLongitudeE6()/1E6;
-		
-		Log.v("DEBUG", "orig:"+orig+" dest:"+dest);
-		
-		// there's a lot that can go wrong here...
-		try {
+        // FrameLayout mapContainer = (FrameLayout) findViewById(R.id.map_container);
+        // RelativeLayout parentContainer = (RelativeLayout) findViewById(R.id.parent_container);
+        FragmentManager fm = this.getSupportFragmentManager();
 
-			// create http client
-			HttpClient hc = new DefaultHttpClient();
-			HttpResponse response;
-			
-			// create uri for terminus points
-			URI uri = new URI("http://wideworld.media.mit.edu/path?lat1="+lat1+"&lon1="+lng1+"&lat2="+lat2+"&lon2="+lng2);
-			Log.v("DEBUG",uri.toString());
-			
-			// grab response
-			response = hc.execute( new HttpGet( uri ));
-			StatusLine statusLine = response.getStatusLine();
-			
-			// if the response is OK
-		    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-		    	// piece together the response string
-		        ByteArrayOutputStream out = new ByteArrayOutputStream();
-		        response.getEntity().writeTo(out);
-		        out.close();
-		        String responseString = out.toString();
-		        Log.v("DEBUG", Integer.toString(responseString.length()));
-		        
-		        // convert the response string into a map overlay
-		        try {
-		        	// the entire message
-					JSONObject object = (JSONObject) new JSONTokener(responseString).nextValue();
-					String id1 = object.getString("id1");
-					String id2 = object.getString("id2");
-					
-					// get list of linestrings
-					JSONArray geoms = object.getJSONArray("geom");
-					
-					// for each linestring
-					for(int i=0; i<geoms.length(); i++){
-						// create path overlay
-						PathOverlay pathoverlay = new PathOverlay(Color.RED,this);
-						
-						// populate path overlay
-						JSONArray geom = geoms.getJSONArray(i);
-						for(int j=0; j<geom.length(); j++){
-							JSONArray pt = geom.getJSONArray(j);
-							double lng = pt.getDouble(0);
-							double lat = pt.getDouble(1);
-							pathoverlay.addPoint(new GeoPoint(lat,lng));
-						}
-						
-						// add path overlay to map
-						mv.getOverlays().add(pathoverlay);
-					}
-					
-					Log.v("DEBUG", "id1:"+id1+" id2:"+id2);
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		    } else{
-		        //Closes the connection.
-		        response.getEntity().getContent().close();
-		        throw new IOException(statusLine.getReasonPhrase());
-		    }
-			//Log.v("DEBUG", "resp:"+response);
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        MapFragment mapFragment = new MapFragment();
 
- catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public class GestureOverlay extends Overlay {
-		
-		Context context;
+        fm.beginTransaction().add(R.id.map_container, mapFragment).commit();
+    }
+    
 
-		public GestureOverlay(Context ctx) {
-			super(ctx);
-			context = ctx;
-		}
-
-		@Override
-		protected void draw(Canvas arg0, MapView arg1, boolean arg2) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-        public boolean onLongPress(final MotionEvent e, final MapView mapView) {
-        	final IGeoPoint pt = mapView.getProjection().fromPixels(e.getX(), e.getY());
-        	Log.v("DEBUG", "poke ("+e.getX()+","+e.getY()+"), projected to ("+pt.getLatitudeE6()+","+pt.getLongitudeE6()+")");
-        	
-        	// build alert dialog
-        	AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        	builder.setPositiveButton("Destination", new DialogInterface.OnClickListener(){
-        		public void onClick(DialogInterface dialog, int id){
-        			dest = pt;
-        			findAndDisplayRoute();
-        		}
-        	});
-        	builder.setNegativeButton("Origin", new DialogInterface.OnClickListener(){
-        		public void onClick(DialogInterface dialog, int id){
-        			orig = pt;
-        			findAndDisplayRoute();
-        		}
-        	});
-        	builder.setCancelable(true);
-        	AlertDialog ad = builder.create();
-        	
-        	// show alert dialog
-        	ad.show();
-        	
-            return false;
-        }
-        
-        
-	}
-	
-
+    
 }
---
+++ frontend/WideWorld/src/edu/mit/media/wideworld/MapFragment.java
@@ -0,0 +1,293 @@
+// Created by plusminus on 00:23:14 - 03.10.2008
+package edu.mit.media.wideworld;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.PathOverlay;
+
+import edu.mit.media.wideworld.constants.OpenStreetMapConstants;
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Default map view activity.
+ * 
+ * @author Marc Kurtz
+ * @author Manuel Stahl
+ * 
+ */
+public class MapFragment extends Fragment implements OpenStreetMapConstants
+{
+
+    // ===========================================================
+    // Fields
+    // ===========================================================
+
+    private SharedPreferences mPrefs;
+    private MapView mMapView;
+    private MinimapOverlay mMinimapOverlay;
+    private ResourceProxy mResourceProxy;
+    private PathOverlay mPathOverlay;
+    
+	private GeoPoint orig;
+	private GeoPoint dest;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+        mMapView = new MapView(inflater.getContext(), 256, mResourceProxy);
+        mMapView.setUseSafeCanvas(true);
+        setHardwareAccelerationOff();
+        return mMapView;
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setHardwareAccelerationOff()
+    {
+        // Turn off hardware acceleration here, or in manifest
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+            mMapView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        final Context context = this.getActivity();
+		final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        // mResourceProxy = new ResourceProxyImpl(getActivity().getApplicationContext());
+
+        mPrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+        mMinimapOverlay = new MinimapOverlay(getActivity(), mMapView.getTileRequestCompleteHandler());
+		mMinimapOverlay.setWidth(dm.widthPixels / 5);
+		mMinimapOverlay.setHeight(dm.heightPixels / 5);
+
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMultiTouchControls(true);
+        mMapView.getOverlays().add(this.mMinimapOverlay);
+        
+        Overlay overlay = new GestureOverlay(context);
+		mMapView.getOverlays().add(overlay);
+
+        mMapView.getController().setZoom(mPrefs.getInt(PREFS_ZOOM_LEVEL, 1));
+        mMapView.scrollTo(mPrefs.getInt(PREFS_SCROLL_X, 0), mPrefs.getInt(PREFS_SCROLL_Y, 0));
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onPause()
+    {
+        final SharedPreferences.Editor edit = mPrefs.edit();
+        edit.putString(PREFS_TILE_SOURCE, mMapView.getTileProvider().getTileSource().name());
+        edit.putInt(PREFS_SCROLL_X, mMapView.getScrollX());
+        edit.putInt(PREFS_SCROLL_Y, mMapView.getScrollY());
+        edit.putInt(PREFS_ZOOM_LEVEL, mMapView.getZoomLevel());
+        edit.commit();
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        final String tileSourceName = mPrefs.getString(PREFS_TILE_SOURCE,
+                TileSourceFactory.DEFAULT_TILE_SOURCE.name());
+        try {
+            final ITileSource tileSource = TileSourceFactory.getTileSource(tileSourceName);
+            mMapView.setTileSource(tileSource);
+        } catch (final IllegalArgumentException ignore) {
+        }
+    }
+
+	protected void startSampleFragment(Fragment fragment) {
+		FragmentManager fm = getFragmentManager();
+		fm.beginTransaction().hide(this).add(android.R.id.content, fragment, "SampleFragment")
+				.addToBackStack(null).commit();
+	}
+	
+	private void findAndDisplayRoute() {
+		final Context context = this.getActivity();
+		
+		if( orig==null || dest==null ){
+			return;
+		}
+		
+		// get the orig and dest points
+		double lat1 = orig.getLatitudeE6()/1E6;
+		double lng1 = orig.getLongitudeE6()/1E6;
+		double lat2 = dest.getLatitudeE6()/1E6;
+		double lng2 = dest.getLongitudeE6()/1E6;
+		
+		Log.v("DEBUG", "orig:"+orig+" dest:"+dest);
+		
+		// there's a lot that can go wrong here...
+		try {
+
+			// create http client
+			HttpClient hc = new DefaultHttpClient();
+			HttpResponse response;
+			
+			// create uri for terminus points
+			URI uri = new URI("http://wideworld.media.mit.edu/path?lat1="+lat1+"&lon1="+lng1+"&lat2="+lat2+"&lon2="+lng2);
+			Log.v("DEBUG",uri.toString());
+			
+			// grab response
+			response = hc.execute( new HttpGet( uri ));
+			StatusLine statusLine = response.getStatusLine();
+			
+			// if the response is OK
+		    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+		    	// piece together the response string
+		        ByteArrayOutputStream out = new ByteArrayOutputStream();
+		        response.getEntity().writeTo(out);
+		        out.close();
+		        String responseString = out.toString();
+		        Log.v("DEBUG", Integer.toString(responseString.length()));
+		        
+		        // convert the response string into a map overlay
+		        try {
+		        	// the entire message
+					JSONObject object = (JSONObject) new JSONTokener(responseString).nextValue();
+					String id1 = object.getString("id1");
+					String id2 = object.getString("id2");
+					
+					// get list of linestrings
+					JSONArray geoms = object.getJSONArray("geom");
+					
+					// for each linestring
+					for(int i=0; i<geoms.length(); i++){
+						// create path overlay
+						PathOverlay pathoverlay = new PathOverlay(Color.RED,context);
+						
+						// populate path overlay
+						JSONArray geom = geoms.getJSONArray(i);
+						for(int j=0; j<geom.length(); j++){
+							JSONArray pt = geom.getJSONArray(j);
+							double lng = pt.getDouble(0);
+							double lat = pt.getDouble(1);
+							pathoverlay.addPoint(new GeoPoint(lat,lng));
+						}
+						
+						// add path overlay to map
+						mMapView.getOverlays().add(pathoverlay);
+					}
+					
+					Log.v("DEBUG", "id1:"+id1+" id2:"+id2);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    } else{
+		        //Closes the connection.
+		        response.getEntity().getContent().close();
+		        throw new IOException(statusLine.getReasonPhrase());
+		    }
+			//Log.v("DEBUG", "resp:"+response);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+ catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public class GestureOverlay extends Overlay {
+		
+		Context context;
+
+		public GestureOverlay(Context ctx) {
+			super(ctx);
+			context = ctx;
+		}
+
+		@Override
+		protected void draw(Canvas arg0, MapView arg1, boolean arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+        public boolean onLongPress(final MotionEvent e, final MapView mapView) {
+        	final GeoPoint pt = (GeoPoint) mapView.getProjection().fromPixels(e.getX(), e.getY());
+        	Log.v("DEBUG", "poke ("+e.getX()+","+e.getY()+"), projected to ("+pt.getLatitudeE6()+","+pt.getLongitudeE6()+")");
+        	
+        	// build alert dialog
+        	AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        	builder.setPositiveButton("Destination", new DialogInterface.OnClickListener(){
+        		public void onClick(DialogInterface dialog, int id){
+        			dest = pt;
+        			findAndDisplayRoute();
+        		}
+        	});
+        	builder.setNegativeButton("Origin", new DialogInterface.OnClickListener(){
+        		public void onClick(DialogInterface dialog, int id){
+        			orig = pt;
+        			findAndDisplayRoute();
+        		}
+        	});
+        	builder.setCancelable(true);
+        	AlertDialog ad = builder.create();
+        	
+        	// show alert dialog
+        	ad.show();
+        	
+            return false;
+        }
+        
+        
+	}
+
+}
