7ef36ade1335aec942940e48acc56e85b16ee2f1
Brandon Martin-Anderson <badhill@gmail.com>
2013-06-21 16:33:11 -0400

    toward using addresses

--- frontend/WideWorld/src/edu/mit/media/wideworld/ControlFragment.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/ControlFragment.java
@@ -1,7 +1,12 @@
 package edu.mit.media.wideworld;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.annotation.TargetApi;
 import android.app.Fragment;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -18,7 +24,7 @@ public class ControlFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View fragView = inflater.inflate(R.layout.control_fragment, container, false);
+		final View fragView = inflater.inflate(R.layout.control_fragment, container, false);
 		
         CheckBox use_transit = (CheckBox)fragView.findViewById(R.id.checkbox_usetransit);
         use_transit.setChecked(true);
@@ -36,6 +42,35 @@ public class ControlFragment extends Fragment {
         final Button button = (Button) fragView.findViewById(R.id.go_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+            	//MainActivity getActivity();
+            	
+            	Geocoder geocoder = new Geocoder(v.getContext());
+            	
+            	/* if there's an address in the origin box, use it as start point */
+            	EditText orig_text_field = (EditText)fragView.findViewById(R.id.orig_text);
+            	String orig_text = orig_text_field.getText().toString();
+            	
+            	if( orig_text.length() > 0 ){
+	            	try {
+	            		//TODO use non-hardcoded bounding box
+						List<Address> addresses = geocoder.getFromLocationName(orig_text, 1, 42.184267, -71.249771, 42.449301, 70.888595);
+						
+//						if( addresses.size() > 0 ){
+//							
+//						}
+						
+						for(int i=0; i<addresses.size(); i++){
+							Address addy = addresses.get(i);
+							Log.v("DEBUG", addy.toString());
+						}
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+            	}
+            	
+            	Log.v("DEBUG", "orig text: "+orig_text.getText() );
+            	
                 Log.v("DEBUG", "CLICK YO");
             }
         });
