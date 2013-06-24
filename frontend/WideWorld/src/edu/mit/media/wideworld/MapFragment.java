741807c0b3c57943b1c7e26cf92fc6fca2722192
Brandon Martin-Anderson <badhill@gmail.com>
2013-06-24 13:40:47 -0400

    refactored the wideworld server into its own class

--- frontend/WideWorld/src/edu/mit/media/wideworld/MapFragment.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/MapFragment.java
@@ -1,24 +1,12 @@
 // Created by plusminus on 00:23:14 - 03.10.2008
 package edu.mit.media.wideworld;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.tileprovider.tilesource.ITileSource;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -37,7 +25,6 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -71,12 +58,15 @@ public class MapFragment extends Fragment implements OpenStreetMapConstants
     private ResourceProxy mResourceProxy;
     private List<PathOverlay> mPathOverlays;
     
+    RouteServer routeServer;
+    
 
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        routeServer = new RouteServer("wideworld.media.mit.edu", "bos");
     }
 
     @Override
@@ -165,13 +155,6 @@ public class MapFragment extends Fragment implements OpenStreetMapConstants
 		final MainActivity context = (MainActivity)this.getActivity();
 		
 	    boolean useTransit = context.useTransit;
-	    
-	    String use_transit_string;
-	    if(useTransit){
-	    	use_transit_string="t";
-	    } else {
-	    	use_transit_string="f";
-	    }
 		
 		if( context.orig==null || context.dest==null ){
 			return;
@@ -182,13 +165,83 @@ public class MapFragment extends Fragment implements OpenStreetMapConstants
 		double lng1 = context.orig.getLongitudeE6()/1E6;
 		double lat2 = context.dest.getLatitudeE6()/1E6;
 		double lng2 = context.dest.getLongitudeE6()/1E6;
+				
+		routeServer.getRoute(routeServer.new Request(lat1, lng1, lat2, lng2, useTransit, 4.5), new RouteServer.FetchRouteCallback(){
+			public void onResponse(RouteServer.Response resp){
+				// remove all the old paths from the map
+				if(mPathOverlays != null){
+					List<Overlay> allOverlays = mMapView.getOverlays();
+					for(int i=0; i<mPathOverlays.size(); i++){
+						allOverlays.remove(mPathOverlays.get(i));
+					}
+				}
+				// prepare the path list to accept new paths
+				mPathOverlays = new ArrayList<PathOverlay>();
+				
+				// get list of legs
+				JSONArray plan;
+				try {
+					plan = resp.rawResponse.getJSONArray("plan");
+				
+					// figure out trip time from first and last leg
+					JSONObject firstLeg = plan.getJSONObject(0);
+					JSONObject lastLeg = plan.getJSONObject(plan.length()-1);
+					
+					JSONArray firstLegLocs = firstLeg.getJSONArray("locs");
+					JSONArray lastLegLocs = lastLeg.getJSONArray("locs");
+					
+					JSONObject firstLoc = firstLegLocs.getJSONObject(0);
+					JSONObject lastLoc = lastLegLocs.getJSONObject(lastLegLocs.length()-1);
+					
+					int firstLocTime = firstLoc.getInt("time");
+					int lastLocTime = lastLoc.getInt("time");
+					
+					Log.v("DEBUG", "total time: "+(lastLocTime-firstLocTime)+"s" );
+					
+					// for each leg
+					for(int i=0; i<plan.length(); i++){
+						JSONObject leg = plan.getJSONObject(i);
+						int pathColor = Color.GRAY;
+						if( leg.getString("type").equals("transit") ) {
+							pathColor = Color.RED;
+						} else if (leg.getString("type").equals("walk") && leg.getString("mode").equals("walk")){
+							pathColor = Color.BLUE;
+						} else if (leg.getString("type").equals("walk") && leg.getString("mode").equals("bikeshare")){
+						    pathColor = Color.GREEN;
+						}
+						
+						// create path overlay
+						PathOverlay pathoverlay = new PathOverlay(pathColor,context);
+						pathoverlay.getPaint().setStrokeWidth(5.0f);
+	
+						// populate path overlay
+						JSONArray leg_locs = leg.getJSONArray("locs");
+						for(int j=0; j<leg_locs.length(); j++){
+							JSONObject loc = leg_locs.getJSONObject(j);
+							double lng = loc.getDouble("lon");
+							double lat = loc.getDouble("lat");
+							pathoverlay.addPoint(new GeoPoint(lat,lng));
+						}	
+	
+						// add path overlay to map
+						mMapView.getOverlays().add(pathoverlay);
+						mPathOverlays.add(pathoverlay);
+	
+					}
+					//mMapView.invalidate();
+					Message msg = new Message();
+					msg.arg1 = firstLocTime;
+					msg.arg2 = lastLocTime;
+					msg.obj = context;
+					mHandler.sendMessage(msg);
+					
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 		
-		Log.v("DEBUG", "orig:"+context.orig+" dest:"+context.dest);
-		
-		String url = "http://wideworld.media.mit.edu/bos/plan?lat1="+lat1+"&lon1="+lng1+"&lat2="+lat2+"&lon2="+lng2+"&bspeed=4.5&transit="+use_transit_string;
-		FetchRouteTask rt = new FetchRouteTask();
-		rt.setContext(context);
-		rt.execute(url);
 	}
 	
 	static Handler mHandler = new Handler() {		
@@ -202,141 +255,6 @@ public class MapFragment extends Fragment implements OpenStreetMapConstants
         }
 
 	};
-	
-	private class FetchRouteTask extends AsyncTask<String, Void, Void> {
-		
-		private Context context;
-
-		@Override
-		protected Void doInBackground(String... url) {
-			
-			// there's a lot that can go wrong here...
-			try {
-
-				// create http client
-				HttpClient hc = new DefaultHttpClient();
-				HttpResponse response;
-				
-				// create uri for terminus points
-				URI uri = new URI(url[0]);
-				Log.v("DEBUG",uri.toString());
-				
-				// grab response
-				response = hc.execute( new HttpGet( uri ));
-				StatusLine statusLine = response.getStatusLine();
-				
-				// if the response is OK
-			    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-			    	// piece together the response string
-			        ByteArrayOutputStream out = new ByteArrayOutputStream();
-			        response.getEntity().writeTo(out);
-			        out.close();
-			        String responseString = out.toString();
-			        
-			        // convert the response string into a map overlay
-			        try {
-			        	// the entire message
-						JSONObject object = (JSONObject) new JSONTokener(responseString).nextValue();
-						
-						
-						// remove all the old paths from the map
-						if(mPathOverlays != null){
-							List<Overlay> allOverlays = mMapView.getOverlays();
-							for(int i=0; i<mPathOverlays.size(); i++){
-								allOverlays.remove(mPathOverlays.get(i));
-							}
-						}
-						// prepare the path list to accept new paths
-						mPathOverlays = new ArrayList<PathOverlay>();
-						
-						// get list of legs
-						JSONArray plan = object.getJSONArray("plan");
-						
-						// figure out trip time from first and last leg
-						JSONObject firstLeg = plan.getJSONObject(0);
-						JSONObject lastLeg = plan.getJSONObject(plan.length()-1);
-						
-						JSONArray firstLegLocs = firstLeg.getJSONArray("locs");
-						JSONArray lastLegLocs = lastLeg.getJSONArray("locs");
-						
-						JSONObject firstLoc = firstLegLocs.getJSONObject(0);
-						JSONObject lastLoc = lastLegLocs.getJSONObject(lastLegLocs.length()-1);
-						
-						int firstLocTime = firstLoc.getInt("time");
-						int lastLocTime = lastLoc.getInt("time");
-						
-						Log.v("DEBUG", "total time: "+(lastLocTime-firstLocTime)+"s" );
-						
-
-						
-						// for each leg
-						for(int i=0; i<plan.length(); i++){
-							JSONObject leg = plan.getJSONObject(i);
-							int pathColor = Color.GRAY;
-							if( leg.getString("type").equals("transit") ) {
-								pathColor = Color.RED;
-							} else if (leg.getString("type").equals("walk") && leg.getString("mode").equals("walk")){
-								pathColor = Color.BLUE;
-							} else if (leg.getString("type").equals("walk") && leg.getString("mode").equals("bikeshare")){
-							    pathColor = Color.GREEN;
-							}
-							
-							// create path overlay
-							PathOverlay pathoverlay = new PathOverlay(pathColor,context);
-							pathoverlay.getPaint().setStrokeWidth(5.0f);
-
-							// populate path overlay
-							JSONArray leg_locs = leg.getJSONArray("locs");
-							for(int j=0; j<leg_locs.length(); j++){
-								JSONObject loc = leg_locs.getJSONObject(j);
-								double lng = loc.getDouble("lon");
-								double lat = loc.getDouble("lat");
-								pathoverlay.addPoint(new GeoPoint(lat,lng));
-							}	
-
-							// add path overlay to map
-							mMapView.getOverlays().add(pathoverlay);
-							mPathOverlays.add(pathoverlay);
-
-						}
-						//mMapView.invalidate();
-						Message msg = new Message();
-						msg.arg1 = firstLocTime;
-						msg.arg2 = lastLocTime;
-						msg.obj = context;
-						mHandler.sendMessage(msg);
-						
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-			    } else{
-			        //Closes the connection.
-			        response.getEntity().getContent().close();
-			        throw new IOException(statusLine.getReasonPhrase());
-			    }
-				//Log.v("DEBUG", "resp:"+response);
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-	 catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			return null;
-		}
-
-		public void setContext(Context context) {
-			this.context = context;
-		}
-		
-	}
 
 	public class GestureOverlay extends Overlay {
 		
---
+++ frontend/WideWorld/src/edu/mit/media/wideworld/RouteServer.java
@@ -0,0 +1,123 @@
+package edu.mit.media.wideworld;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class RouteServer {
+	String host;
+	String instance;
+	
+	public class Request{
+		public double lat1;
+		public double lng1;
+		public double lat2;
+		public double lng2;
+		public boolean useTransit;
+		public double bikeSpeed;
+		
+		Request(double lat12, double lng12, double lat22, double lng22, boolean useTransit, double bikeSpeed){
+			this.lat1=lat12;
+			this.lng1=lng12;
+			this.lat2=lat22;
+			this.lng2=lng22;
+			this.useTransit = useTransit;
+			this.bikeSpeed = bikeSpeed;
+		}
+		
+		Request(double lat12, double lng12, double lat22, double lng22){
+			this(lat12,lng12,lat22,lng22,true,2.0);
+		}
+	}
+	
+	public class Response{
+		public JSONObject rawResponse;
+	}
+	
+	public interface FetchRouteCallback {
+		void onResponse(Response resp);
+	}
+	
+	RouteServer( String host, String instance ){
+		this.host = host;
+		this.instance = instance;
+	}
+	
+	void getRoute( Request request, final FetchRouteCallback callback ){
+		class FetchRouteTask extends AsyncTask<String, Void, Void> {
+			
+			@Override
+			protected Void doInBackground(String... url) {
+				
+				// there's a lot that can go wrong here...
+				try {
+
+					// create http client
+					HttpClient hc = new DefaultHttpClient();
+					HttpResponse response;
+					
+					// create uri for terminus points
+					URI uri = new URI(url[0]);
+					
+					// grab response
+					response = hc.execute( new HttpGet( uri ));
+					StatusLine statusLine = response.getStatusLine();
+					
+					// if the response is OK
+				    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+				    	// piece together the response string
+				        ByteArrayOutputStream out = new ByteArrayOutputStream();
+				        response.getEntity().writeTo(out);
+				        out.close();
+				        String responseString = out.toString();
+				        
+				        JSONObject object = (JSONObject) new JSONTokener(responseString).nextValue();
+				        Response resp = new Response();
+				        resp.rawResponse = object;
+				        
+				        callback.onResponse( resp );
+				        
+				        
+				    } else{
+				        //Closes the connection.
+				        response.getEntity().getContent().close();
+				        throw new IOException(statusLine.getReasonPhrase());
+				    }
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				return null;
+			}
+			
+		}
+		String url = "http://"+this.host+"/"+this.instance+"/plan?lat1="+request.lat1+"&lon1="+request.lng1+"&lat2="+request.lat2+"&lon2="+request.lng2+"&bspeed="+request.bikeSpeed+"&transit="+(request.useTransit?"t":"f");
+		FetchRouteTask rt = new FetchRouteTask();
+		rt.execute(url);
+	}
+}
