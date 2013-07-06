b510f7305b77adfb6d89c1b00864c381f30cd89d
Brandon Martin-Anderson <badhill@gmail.com>
2013-07-06 17:37:06 -0400

    locpicker item much better

--- frontend/WideWorld/src/edu/mit/media/wideworld/AddressAdapter.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/AddressAdapter.java
@@ -2,10 +2,13 @@ package edu.mit.media.wideworld;
 
 import java.util.List;
 
+import android.content.Context;
 import android.location.Address;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 class AddressAdapter extends BaseAdapter {
@@ -40,10 +43,30 @@ class AddressAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int pos, View convert, ViewGroup parent) {
-		TextView foo = new TextView(parent.getContext());
+		LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LinearLayout item = (LinearLayout) inflater.inflate( R.layout.locpicker_item, parent, false );
+		TextView line1 = (TextView) item.findViewById(R.id.line1);
+		TextView line2 = (TextView) item.findViewById(R.id.line2);
 		Address address = addresses.get(pos);
-		foo.setText( address.getAddressLine(0) + ", " + address.getAddressLine(1) );
-		return foo;
+		line1.setText( address.getAddressLine(0) );
+		line2.setText( getSecondLine(address) );
+		return item;
+	}
+
+	private String getSecondLine(Address address) {
+		String secondLine = "";
+		if(address.getLocality() != null){
+			secondLine += address.getLocality();
+		}
+		if(address.getSubAdminArea() != null){
+			if(secondLine.length()>0){ secondLine += ", "; }
+			secondLine += address.getSubAdminArea();
+		}
+		if(address.getAdminArea() != null){
+			if(secondLine.length()>0){ secondLine += ", "; }
+			secondLine += address.getAdminArea();
+		}
+		return secondLine;
 	}
 
 	public void setAddresses(List<Address> addresses) {
