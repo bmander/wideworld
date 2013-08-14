5ce4ad27c9d342f918f5587aa01f1fbea80d186f
Brandon Martin-Anderson <badhill@gmail.com>
2013-08-14 18:40:00 -0400

    towards setup screen

--- frontend/WideWorld/src/edu/mit/media/wideworld/SetupActivity.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/SetupActivity.java
@@ -1,14 +1,18 @@
 package edu.mit.media.wideworld;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 
 public class SetupActivity extends Activity {
 
-    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setup_activity);
+        setFinishOnTouchOutside(false);
     }
     
 }
