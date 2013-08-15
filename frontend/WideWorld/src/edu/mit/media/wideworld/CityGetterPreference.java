97c31a7c1e167e83ddd5a676f005955908f3f047
Brandon Martin-Anderson <badhill@gmail.com>
2013-08-14 22:10:49 -0400

    city getter

--- frontend/WideWorld/src/edu/mit/media/wideworld/CityGetterPreference.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/CityGetterPreference.java
@@ -1,21 +1,8 @@
 package edu.mit.media.wideworld;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.DialogPreference;
@@ -29,90 +16,6 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class CityGetterPreference extends DialogPreference{
-	
-	class GetCitiesTask extends AsyncTask<Void,Void,Void> {
-		
-		private static final int GETCITY_SUCCESS = 0;
-		private static final int GETCITY_FAIL = 1;
-		private Handler handler;
-		
-		GetCitiesTask(Handler handler){
-			this.handler = handler;
-		}
-		
-
-		
-		String getInstancesJSON( ) throws IOException {
-			
-			
-			// there's a lot that can go wrong here...
-			try {
-
-				// create http client
-				HttpClient hc = new DefaultHttpClient();
-				HttpResponse response;
-				
-				// create uri for terminus points
-				URI uri = new URI( "http://wideworld.media.mit.edu/instances" );
-				
-				// grab response
-				response = hc.execute( new HttpGet( uri ));
-				StatusLine statusLine = response.getStatusLine();
-				
-				// if the response is OK
-			    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-			    	// piece together the response string
-			        ByteArrayOutputStream out = new ByteArrayOutputStream();
-			        response.getEntity().writeTo(out);
-			        out.close();
-			        String responseString = out.toString();
-			        return responseString;
-			        
-			    } else{
-			        //Closes the connection.
-			        response.getEntity().getContent().close();
-			        throw new IOException(statusLine.getReasonPhrase());
-			    }
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			} 
-			
-			return null;
-			
-		}
-		
-		@Override
-		protected Void doInBackground(Void... params) {
-			try {
-				String instancesJSON = this.getInstancesJSON();
-	
-				if( instancesJSON == null ){
-					Message msg = new Message();
-					msg.arg1 = GETCITY_FAIL;
-					handler.sendMessage(msg);
-				} else {
-					Message msg = new Message();
-					msg.arg1 = GETCITY_SUCCESS;
-					msg.obj = instancesJSON;
-					handler.sendMessage(msg);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-				Message msg = new Message();
-				msg.arg1 = GETCITY_FAIL;
-				handler.sendMessage(msg);
-			}
-			
-			return null;
-		}
-		
-	}
-
-
 	
 	static class GetCitiesHandler extends Handler {
 		
--- frontend/WideWorld/src/edu/mit/media/wideworld/SetupActivity.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/SetupActivity.java
@@ -1,20 +1,70 @@
 package edu.mit.media.wideworld;
 
+import java.util.List;
+
+import edu.mit.media.wideworld.CityGetterPreference.GetCitiesHandler;
+
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
 
 public class SetupActivity extends Activity {
+	
+	static class GetCitiesHandler extends Handler {
+		
+		private SetupActivity ctx;
+		
+		GetCitiesHandler(SetupActivity ctx){
+			super();
+
+			this.ctx = ctx;
+			
+		}
+		
+		@Override
+	    public void handleMessage(Message msg) {	    	
+	    	
+	    	if( msg.arg1 == GetCitiesTask.GETCITY_FAIL ){
+	    		// in the case of failure, enable a 'retry' button
+	    	} else {	    		
+
+	    		String citiesJSON = (String)msg.obj;
+	    		List<CityInstance> cities = CitiesFile.getInstances(citiesJSON);
+	    		
+	    		if( cities!=null ){
+	    			ctx.showCitiesList(cities);
+	    		}
+	    			
+	    		CitiesFile.saveInstancesJSON(ctx, citiesJSON);
+				
+	    	}
+		    	
+	    	ctx.progress.setVisibility(View.INVISIBLE);
+			
+	    }
+
+
+	}
 	
 	View pitch;
 	View consentForm;
 	View cityPicker;
+	
+	LinearLayout cityList;
+	ProgressBar progress;
+	
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
@@ -47,12 +97,54 @@ public class SetupActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				setContentView(cityPicker);
+				
+				// todo move this to start when setup starts
+				new GetCitiesTask(new GetCitiesHandler(SetupActivity.this)).execute();
 			}	
 		});
 		
+		cityList = (LinearLayout) cityPicker.findViewById(R.id.citylist);
+		progress = (ProgressBar) cityPicker.findViewById(R.id.progressbar);
+		
 		setContentView( pitch );
+		
+		
 
 		setFinishOnTouchOutside(false);
+		
     }
+
+
+	public void showCitiesList(List<CityInstance> cities) {
+		Log.v("DEBUG", "write list of "+cities.size()+" cities");
+		
+		cityList.removeAllViews();
+		
+		for(int i=0; i<cities.size(); i++){
+			RadioButton cityView = new RadioButton(this);
+			cityView.setLayoutParams( new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT) );
+			cityView.setText(cities.get(i).city_name);
+			cityView.setTag(cities.get(i).prefix);
+			cityView.setPadding(5, 10, 10, 5);
+			cityView.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					String prefix = (String)v.getTag();
+					
+					// set preference
+					Log.v("DEBUG", "preferred city is "+prefix);
+				}
+				
+			});
+			cityList.addView(cityView);
+			
+//			if(cities.get(i).prefix.equals(clickedPrefix)){
+//				citylist.check(cityView.getId());
+//			}
+		}
+		
+//		this.cities = cities;
+	}
     
 }
