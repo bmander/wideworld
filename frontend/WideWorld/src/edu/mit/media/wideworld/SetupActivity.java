e9810539e96b1d5eae612f8ba24514e4c9530653
Brandon Martin-Anderson <badhill@gmail.com>
2013-08-14 21:39:23 -0400

    building out the setup activity

--- frontend/WideWorld/src/edu/mit/media/wideworld/SetupActivity.java
+++ frontend/WideWorld/src/edu/mit/media/wideworld/SetupActivity.java
@@ -4,15 +4,55 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SetupActivity extends Activity {
+	
+	View pitch;
+	View consentForm;
+	View cityPicker;
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setup_activity);
-        setFinishOnTouchOutside(false);
+        
+		LayoutInflater inflater = getLayoutInflater();
+		pitch = inflater.inflate(R.layout.setup_activity, null);
+		consentForm = inflater.inflate(R.layout.consent_form, null);
+		cityPicker = inflater.inflate(R.layout.city_picker, null);
+		
+		Button pitchYes = (Button)pitch.findViewById(R.id.yesbutton);
+		pitchYes.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				setContentView( consentForm );
+			}
+		});
+		
+		Button pitchNo = (Button)pitch.findViewById(R.id.nobutton);
+		pitchNo.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setContentView(cityPicker);
+			}
+		});
+		
+		Button consentDone = (Button)consentForm.findViewById(R.id.yesbutton);
+		consentDone.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setContentView(cityPicker);
+			}	
+		});
+		
+		setContentView( pitch );
+
+		setFinishOnTouchOutside(false);
     }
     
 }
